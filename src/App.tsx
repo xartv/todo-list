8596ad05c4664f16dd0192b84dc118e3893445cf
Artem Vasilev <artemvasilev@MacBook-Pro-Artem.local>
2023-05-14 21:14:06 +0400

    Add basic functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import { Todo } from "./types";
+
+import s from "./App.module.css";
+
+const mockTodos: Todo[] = [
+  { title: "Learn redux", id: "1", completed: false },
+  { title: "Write code", id: "2", completed: false },
+  { title: "Profit", id: "3", completed: false },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = React.useState("");
+  const [todos, setTodos] = React.useState<Todo[]>(mockTodos);
+
+  const onClick = () => {
+    if (!value) return;
+
+    const newTodo: Todo = {
+      title: value,
+      id: uuidv4(),
+      completed: false,
+    };
+
+    setTodos((prev) => [...prev, newTodo]);
+    setValue("");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={s.container}>
+      <div className={s.controls}>
+        <input
+          type="text"
+          value={value}
+          className={s.input}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={onClick}>Add todo</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <ul className={s.todoWrapper}>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
