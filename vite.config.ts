4867f088a90cc699607b69a52fa67e072d738f8c
Artem Vasilev <xartvdev@gmail.com>
2023-06-09 00:50:37 +0400

    remove unused imports

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import eslintPlugin from 'vite-plugin-eslint';
-import stylelintPlugin from 'vite-plugin-stylelint';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
