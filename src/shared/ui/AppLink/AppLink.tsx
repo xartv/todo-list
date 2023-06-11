7726527514a17da9f3af4268a92ff4a846811c1d
Artem Vasilev <xartvdev@gmail.com>
2023-06-11 20:04:18 +0400

    add shared AppLink component

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Link, LinkProps } from 'react-router-dom';
+import cn from 'classnames';
+
+import s from './AppLink.module.scss';
+
+export enum AppLinkTheme {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+}
+
+interface AppLinkProps extends LinkProps {
+  className?: 'string';
+  theme?: AppLinkTheme;
+  children: React.ReactNode;
+}
+
+export const AppLink = ({ className, theme = AppLinkTheme.PRIMARY, to, children, ...otherProps }: AppLinkProps) => {
+  return (
+    <Link to={to} className={cn(className, s[theme])} {...otherProps}>
+      {children}
+    </Link>
+  );
+};
--- src/widgets/AppHeader/ui/AppHeader.tsx
+++ src/widgets/AppHeader/ui/AppHeader.tsx
@@ -1,12 +1,17 @@
-import { Link } from 'react-router-dom';
+import { AppLink } from 'shared/ui/AppLink';
 
 import s from './AppHeader.module.scss';
+import { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 
 export const AppHeader = () => {
   return (
     <div className={s.root}>
-      <Link to="/todos">Todos</Link>
-      <Link to="/test">Test Page</Link>
+      <AppLink to="/todos" theme={AppLinkTheme.SECONDARY}>
+        Todos
+      </AppLink>
+      <AppLink to="/test" theme={AppLinkTheme.SECONDARY}>
+        Test Page
+      </AppLink>
     </div>
   );
 };
