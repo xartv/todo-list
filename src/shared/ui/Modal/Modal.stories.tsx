ebf447d5613a5e7b5f9a633a7f8550e9888a3aa4
Artem Vasilev <xartvdev@gmail.com>
2023-06-21 23:28:28 +0400

    fix issues with styles in storybook

---
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Theme } from 'app/providers/ThemeProvider/lib/ThemeContext';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
+import { Modal } from './Modal';
+
+const meta: Meta<typeof Modal> = {
+  component: Modal,
+};
+
+export default meta;
+type Story = StoryObj<typeof Modal>;
+
+export const Primary: Story = {
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, odit! Pariatur, expedita minus quam nesciunt laudantium minima rem laborum, fuga earum, excepturi saepe. Earum minima aspernatur ut officia laudantium accusamus.',
+    isOpen: true,
+    className: 'app_theme_light',
+  },
+};
+
+export const PrimaryDark: Story = {
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, odit! Pariatur, expedita minus quam nesciunt laudantium minima rem laborum, fuga earum, excepturi saepe. Earum minima aspernatur ut officia laudantium accusamus.',
+    isOpen: true,
+    className: 'app_theme_dark',
+  },
+  decorators: [ThemeDecorator(Theme.DARK)],
+};
--- src/widgets/PageError/ui/PageError.stories.tsx
+++ src/widgets/PageError/ui/PageError.stories.tsx
@@ -13,8 +13,15 @@ const meta: Meta<typeof PageError> = {
 export default meta;
 type Story = StoryObj<typeof PageError>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: {
+    className: 'app_theme_light',
+  },
+};
 
 export const PrimaryDark: Story = {
+  args: {
+    className: 'app_theme_dark',
+  },
   decorators: [ThemeDecorator(Theme.DARK)],
 };
