85a9338c82c15d0579fa452a7082f697a1995ac7
Artem Vasilev <xartvdev@gmail.com>
2023-06-25 23:33:03 +0400

    fix lint styles error

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,15 +1,11 @@
-import cn from 'classnames';
-
 import { Modal } from 'shared/ui/Modal';
 import { ModalProps } from 'shared/ui/Modal/Modal';
 
 import { LoginForm } from '../LoginForm/LoginForm';
 
-import s from './LoginModal.module.scss';
-
 export const LoginModal = ({ className, ...modalProps }: Omit<ModalProps, 'children'>) => {
   return (
-    <div className={cn(s.root, className)}>
+    <div className={className}>
       <Modal {...modalProps}>
         <LoginForm />
       </Modal>
