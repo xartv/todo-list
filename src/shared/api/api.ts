f13ab898cffaf58c614829ee8967b225708e1fc0
Artem Vasilev <xartvdev@gmail.com>
2023-09-07 21:41:06 +0400

    fix authorization issue

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,5 +4,11 @@ import { LOCAL_STORAGE_AUTH_USER } from 'shared/const/globalConsts';
 
 export const _api = axios.create({
   baseURL: __APP_BASE_URL__,
-  headers: { Authorization: localStorage.getItem(LOCAL_STORAGE_AUTH_USER) || '' },
+});
+
+_api.interceptors.request.use(config => {
+  if (config.headers) {
+    config.headers.Authorization = localStorage.getItem(LOCAL_STORAGE_AUTH_USER) || '';
+  }
+  return config;
 });
