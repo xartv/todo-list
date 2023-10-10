91cd000b1c0390f9ae8e61314b8a1596af1642a0
Artem Vasilev <xartvdev@gmail.com>
2023-10-10 23:39:16 +0400

    fix storybook preview.tsx

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,10 @@
 import type { Preview } from '@storybook/react';
 
-import { Theme } from '../src/app/providers/ThemeProvider/lib/ThemeContext';
 import { RouteDecorator } from '../src/shared/config/storybook/RouteDecorator/RouteDecorator';
 import { StyleDecorator } from '../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 import { ThemeDecorator } from '../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { TranslationDecorator } from '../src/shared/config/storybook/TranslationDecorator/TranslationDecorator';
+import { Theme } from '../src/shared/const/theme';
 
 import i18n from './i18next';
 
